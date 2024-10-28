ccae2477796d1b7fa94c5b6a560f04c28caf9d5c
Vinura Dulaksith <vinura@azbow.com>
2024-10-28 11:34:39 +0530

    schemna

--- articleSch.js
+++ articleSch.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 
 const articleSchema = new mongoose.Schema({
-    title: {type: String, required:true},
-    author: {type: String, required:true},
-    description: {type: String, required:true},
-    imgUrl: {type: String, required:true},
-    university: {type: String},
-    date: {type:String}
+    articleDetails: [
+        {
+            title: { type: String, required: true },
+            author: { type: String, required: true },
+            description: { type: String, required: true },
+            imgUrl: { type: String, required: true },
+            university: { type: String },
+            date: { type: String }
+        }
+    ]
 });
 
 module.exports = mongoose.model('articleSch',articleSchema);
