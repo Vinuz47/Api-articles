79f9e4218c15c8b4df5abaf493de84702414b9c9
Vinura Dulaksith <vinura@azbow.com>
2024-10-28 11:26:12 +0530

    details articles

--- index.js
+++ index.js
@@ -51,27 +51,29 @@ async function run() {
     try{
         const newArticle = await article.create(
             
+            {
+             articleDetails :   [
+                    {
+                     title:" vfd",
+                     author: "yahoo",
+                     description:"vsdfbsfnbn",
+                     imgUrl: "frgenedne",
+                     university: "fbsb",
+                     date:"dnvjin"
+                    },
+                    {
+                     title:" fwgwgw",
+                     author: "geewgweg",
+                     description:"wergweg",
+                     imgUrl: "ewgfgwe",
+                     university: "grwjrjr",
+                     date:"gehehethe"
+                    }
+     
+     
+                 ]
+            }
             
-            [
-               {
-                title:" vfd",
-                author: "yahoo",
-                description:"vsdfbsfnbn",
-                imgUrl: "frgenedne",
-                university: "fbsb",
-                date:"dnvjin"
-               },
-               {
-                title:" fwgwgw",
-                author: "geewgweg",
-                description:"wergweg",
-                imgUrl: "ewgfgwe",
-                university: "grwjrjr",
-                date:"gehehethe"
-               }
-
-
-            ]
           
     );
         console.log(newArticle);
