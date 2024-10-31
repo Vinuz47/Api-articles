50f88d5a9b1a9d5aabf82a445fad17590147c9e8
Vinura Dulaksith <vinura@azbow.com>
2024-10-31 17:34:19 +0530

    done

--- index.js
+++ index.js
@@ -20,7 +20,17 @@ mongoose.connect(`mongodb+srv://kandageTest:kkkkkk@cluster0.jkks3.mongodb.net/${
 
 // api link : https://apiserverarticles-hsyry33i.b4a.run/all
 
+// Allow CORS from specific origin
+app.use(cors({
+    origin: 'https://vinuz47.github.io'
+}));
 
+// Or, if you prefer setting it manually without `cors` package
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://vinuz47.github.io");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/all',async (req,res)=>{
     try{
